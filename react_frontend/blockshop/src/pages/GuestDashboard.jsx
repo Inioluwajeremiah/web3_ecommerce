35a34fb1940b91396226391b9520cf79583799b2
Inioluwajeremiah <adewarainioluwa@gmail.com>
2022-12-16 13:06:00 +0000

    inital commit

---
+++ react_frontend/blockshop/src/pages/GuestDashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const GuestDashboard = () => {
+  return (
+    <div>
+        <section>
+            <h1>Cart</h1>
+        </section>
+        <section>
+            <h1>Order History</h1>
+        </section>
+        <section>
+            <h1>Shipping</h1>
+        </section>
+    </div>
+  )
+}
+
+export default GuestDashboard
