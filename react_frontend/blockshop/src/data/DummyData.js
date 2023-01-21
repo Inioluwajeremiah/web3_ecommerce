f7f208128aa54c99a2cc8687cbe74d859a683160
Inioluwajeremiah <adewarainioluwa@gmail.com>
2023-01-21 20:23:58 +0100

    updte

---
+++ react_frontend/blockshop/src/data/DummyData.js
@@ -0,0 +1,17 @@
+export const electronicsDummyData =  [
+    image, name, quantity, priceTag, priceDiscount, priceDiscountPercentage
+
+    // compute discount after sale and  bulk sales is more than 300 pieces
+    {
+        image: image,
+        name: name,
+        quantity: quantity,
+        priceTag: priceTag,
+        discount: priceDiscount,
+        discountPercent: priceDiscountPercentage,
+        discription: description,
+        status: ""
+
+    }
+
+]
